d74719ac2bff1831b27d0e6def0735e1256632fd
Henny Feliz <mingito.2017@gmail.com>
2024-12-22 22:29:12 -0400

    .

---
+++ back/microsrv/src/main/java/com/microsrv/MicrosrvApplication.java
@@ -0,0 +1,13 @@
+package com.microsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosrvApplication.class, args);
+	}
+
+}
---
+++ back/microsrv/src/test/java/com/microsrv/MicrosrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
